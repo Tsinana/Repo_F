b8135886251e7458ead6f442baf0aea71dbf2d70
Tsinana <hlebusru@gmail.com>
2022-03-14 20:15:22 +0300

    Сделал № 11 - 12

---
+++ LB_5/Task_12/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+/// Функция принимающая аргументом ответ пользователя
+let ansmanager (st:int):unit = 
+    match st with
+    |_ when st=1||st=2 -> printfn "Подлиза"
+    |_ when st=3 -> printfn "Булка"
+    |_ -> printfn "Дурачок"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Какой твой любимый ЯП?"
+    printfn "1. F#"
+    printfn "2. Prolog"
+    printfn "3. Точно не эта парочка!"
+    let ans = System.Convert.ToInt32(System.Console.ReadLine())
+    printfn "Суперпозиция"
+    ansmanager ans
+    System.Console.WriteLine()
+    printfn "Каррирование*"
+    if ans=1|| ans=2 then printfn "Подлиза"
+    elif ans=3 then printfn "Булка"
+    else printfn "Дурачок"
+    0
---
+++ LB_5/task_11/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+/// Функция принимающая аргументом ответ пользователя
+let ans (st:int):unit = 
+    System.Console.WriteLine()
+    match st with
+    |_ when st=1||st=2 -> printfn "Подлиза"
+    |_ -> printfn "Булка"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Какой твой любимый ЯП?"
+    printfn "1. F#"
+    printfn "2. Prolog"
+    printfn "3. Точно не эта парочка!"
+    System.Convert.ToInt32(System.Console.ReadLine()) |> ans |> System.Console.WriteLine
+    0
