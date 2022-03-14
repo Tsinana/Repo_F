b9254a0542778cc02a61bda1a4c84f80f7b79965
Tsinana <hlebusru@gmail.com>
2022-03-14 23:14:37 +0300

    Победил №14

---
+++ LB_5/Task_14/Program.fs
@@ -0,0 +1,24 @@
+﻿(*Задание 14. Написать функцию обход делителей числа, которая
+выполняет операции над делителями числа, принимает три аргумента,
+число, функция (например, сумма, произведение, минимум, максимум) и
+инициализирующее значение. Функция должна иметь два Int аргумента и
+возвращать Int.
+*)
+
+open System
+
+let rec divider_rc (a:int,init:int,beg:int,func: int->int->int):int = 
+    match beg with
+    | _ when beg>a/2 ->init
+    | _ when a%beg=0 -> divider_rc (a,func beg init,beg+1,func)
+    | _ -> divider_rc (a,init,beg+1,func)
+
+
+let UberFunc (a:int,init:int,func: int->int->int):int = divider_rc (a,init,1,func)
+
+[<EntryPoint>]
+let main argv =
+    let a = System.Convert.ToInt32(System.Console.ReadLine())
+    printfn $"{UberFunc(a,0,fun a b->a+b)}"
+    printfn $"{UberFunc(a,1,fun a b->a*b)}"
+    0
