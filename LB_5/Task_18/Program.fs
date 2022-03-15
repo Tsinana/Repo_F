a3d19a0ba00e585294765fdd5a3b223c8d5baa69
Tsinana <hlebusru@gmail.com>
2022-03-15 21:10:33 +0300

    Метод 3

--- LB_5/Task_18/Program.fs
+++ LB_5/Task_18/Program.fs
@@ -32,6 +32,7 @@ let UberFunc a = divider_rc a 0 1
 
 //---метод 2---
 
+///Поиск максимальной цифры числа, не делящуюся на 3
 let rec Search a (max:int) = 
     if a = 0 then max
     elif ((a%10)>max)&&((a%10%3<>0)) then Search (a/10) (a%10)
@@ -39,6 +40,37 @@ let rec Search a (max:int) =
 
 ///Оболочка для Search
 let ShellForSearch a = Search a -1
+
+//---метод 3---
+
+///Ф-ия поиска наименьшего делителя исходно числа
+let rec minDel a min =
+    if a%min = 0 then min
+    else minDel a (min+1)
+
+/// Оболочка для minDel
+let ShellminDel a = minDel a 2
+
+///Ф-ия поиска суммы цифр числа, меньших 5
+let rec SumNumWhichAreSmallerThan5 a sum =
+    if a=0 then sum
+    elif (a%10)<5 then SumNumWhichAreSmallerThan5 (a/10) (sum+(a%10))
+    else SumNumWhichAreSmallerThan5 (a/10) sum
+
+///Оболочка SumNumWhichAreSmallerThan5
+let ShellForSumNumWhichAreSmallerThan5 a = SumNumWhichAreSmallerThan5 a 0
+
+///метод 3 
+let rec MegaSearch a beg maxbeg sumnum=
+    if beg>a then 
+        printfn $"Максимальное число удов. условию - {maxbeg}"
+        printfn $"Сумма цифр числа, меньших 5 - {ShellForSumNumWhichAreSmallerThan5 a}"
+        maxbeg*(ShellForSumNumWhichAreSmallerThan5 a)
+    elif (NOD a beg <> 1)&&(beg%sumnum <> 0) then MegaSearch a (beg+1) beg sumnum
+    else MegaSearch a (beg+1) maxbeg sumnum
+
+///Оболочка для метода 3
+let  MegaShellForMegaSearch a = MegaSearch a 1 0 (ShellminDel a)
 
 [<EntryPoint>]
 let main argv =
@@ -49,5 +81,6 @@ let main argv =
     if (ShellForSearch a) <> -1 then printfn $"Максимальная цифра числа, не делящуюся на 3 - {ShellForSearch a}"
     else printfn $"Максимальная цифра числа, не делящуюся на 3 - NULL"
     printfn ""
-
+    printfn $"метод 3 - {MegaShellForMegaSearch a}"
+    printfn ""
     0
