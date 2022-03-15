dcaca9d7c47ad4352b6f5a5b7251d48f3bd92cfe
Tsinana <hlebusru@gmail.com>
2022-03-15 17:57:34 +0300

    Сделал 15

---
+++ LB_5/Task_15/Program.fs
@@ -0,0 +1,26 @@
+﻿(*Задание 15. Написать функцию обход взаимно простых компонентов
+числа, которая выполняет операции над числами, взаимно простыми с
+данным, принимает три аргумента, число, функция (например, сумма,
+произведение, минимум, максимум, количество) и инициализирующее
+значение. Функция должна иметь два Int аргумента и возвращать Int*)
+
+open System
+
+let rec NOD a b =
+    if a%b=0 then b
+    else NOD b (a%b)
+
+let rec divider_rc (a:int,init:int,beg:int,func: int->int->int):int = 
+    match beg with
+    | _ when beg>a ->init
+    | _ when (NOD a beg) = 1 -> divider_rc (a,func beg init,beg+1,func)
+    | _ -> divider_rc (a,init,beg+1,func)
+
+let UberFunc (a:int,init:int,func: int->int->int):int = divider_rc (a,init,1,func)
+
+[<EntryPoint>]
+let main argv =
+    let a = System.Convert.ToInt32(System.Console.ReadLine())
+    printfn $"{UberFunc(a,0,fun a b->a+b)}"
+    printfn $"{UberFunc(a,1,fun a b->a*b)}"
+    0
