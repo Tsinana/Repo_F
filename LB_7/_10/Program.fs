3d39c55798593dcf140efc4384d24995791ed00a
Tsinana <hlebusru@gmail.com>
2022-04-20 13:11:53 +0300

    набрался опыта и улучшил прогу + убрал грязь

--- LB_7/_10/Program.fs
+++ LB_7/_10/Program.fs
@@ -1,5 +1,12 @@
 ﻿(*Даны два массива. Необходимо найти количество совпадающих по
-значению элементов.*)
+значению элементов.
+
+Далее в задаче реализовано два подхода к осмыслению задачи 
+Пусть L1 = [1,1,1] L2 = [1,1]
+1. func and func1 дадут ответ - 6
+2. func2 - 2
+*)
+
 
 open System
 
@@ -26,42 +33,32 @@ let rec write_list = function
 
 ///функция решения задачи
 let func list1 list2 =
-    let timer = System.Diagnostics.Stopwatch.StartNew()
-    Console.WriteLine("Количество совпадающих по значению элементов = ")
     (list1,list2)||>List.allPairs|>List.filter (fun (a,b)-> a=b)|>List.length|>Console.WriteLine
-    timer.Stop()
-    printfn "elapsed=%O" timer.Elapsed
-
 
 ///Тоже самое что и func, но избегаем ситуации "Ты хоть понимаешь что будет если у тебя будут 2 листа с +100 элементов?!"
 let func1 list1 list2 = 
-    let timer = System.Diagnostics.Stopwatch.StartNew()
     let rec func1_rec list1 list2 quantity =
         match list1 with
         |[]->quantity
         |h::t-> 
             let quantity1 = quantity + List.length (List.filter (fun x -> x = h) list2)
             func1_rec t list2 quantity1
-    Console.WriteLine("Количество совпадающих по значению элементов = ")
     func1_rec list1 list2 0|> Console.WriteLine
-    timer.Stop()
-    printfn "elapsed=%O" timer.Elapsed
+
+///Функция моего интереса 
+let func2 list1 list2 = 
+    let list3 = List.append list1 list2
+    List.length(List.filter (fun x -> ((List.length (List.filter (fun elem -> elem = x) list3) > 1))) list3)/2|>Console.WriteLine
 
 [<EntryPoint>]
 let main argv =
-    //let list3 = init_list()
-    //let list4 = init_list()
-    let list1 = [1;2;3;4;5;] 
-    let list2 = [9;8;7;6;5;4;3;2;1]
+    let list1 = init_list()
+    let list2 = init_list()
+    Console.WriteLine("Количество совпадающих по значению элементов = ")
     func list1 list2
+    Console.WriteLine("Количество совпадающих по значению элементов = ")
     func1 list1 list2
-    //init_list()|>Console.WriteLine
-    (*
-    Количество совпадающих по значению элементов =
-    5
-    elapsed=00:00:00.0492575 - за такое -1 балл
-    Количество совпадающих по значению элементов =
-    5
-    elapsed=00:00:00.0036955 - за такое +1 балл
-    *)
+    Console.WriteLine("Количество совпадающих по значению элементов = ")
+    func2 list1 list2
+    
     0
