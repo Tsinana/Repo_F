66b46eacfad63c66a376790b012b7a31317a94d2
Tsinana <hlebusru@gmail.com>
2022-05-06 22:47:40 +0300

    Метод 2 - готов

--- LB_7/_19/Program.fs
+++ LB_7/_19/Program.fs
@@ -1,7 +1,13 @@
-﻿(*Дана строка. Необходимо проверить, является ли она палиндромом.*)
+﻿(*Дана строка. Необходимо проверить, является ли она палиндромом.
+
+Дана строка в которой записаны слова через пробел. Необходимо
+посчитать количество слов.
+
+*)
 
 open System
- 
+
+///Строка полиндром?
 let func_1 str1 = 
     let ln = (String.length str1)
     let rec func_1_rec (str1:string) i str2:string =
@@ -20,12 +26,25 @@ let func_1 str1 =
             
     func_1_rec1 str1 0 str2
 
+///Считает колво слов через пробел
+let func_2 str1 =
+    (str1|>String.filter(fun c-> c = ' ')|>String.length)+1
 
+///Ф-ия выбора,а над название потом поработаю
+let ololo str1 a =
+    match a with
+    |1-> func_1 str1|>Console.WriteLine
+    |2-> func_2 str1|>Console.WriteLine
+    |3 -> func_3 str1|>Console.WriteLine
+    |_->Console.WriteLine("ololo")
 
 [<EntryPoint>]
 let main argv =
-    let str1 = Console.ReadLine()
-    func_1 str1|>Console.WriteLine
-    
-
+    Console.WriteLine("Введите строку:")
+    let string0 = Console.ReadLine()
+    Console.WriteLine("Мы -- люди свободные, так что имеем право выбирать!\n
+    1. Узнать яв-ся ли слово полиндромом\n
+    2. А сколько в строке словечек?\n
+    3. Посчитать колво количество различных цифр в его десятичной записи\n")
+    ololo string0|>ignore
     0 // return an integer exit code
