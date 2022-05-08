af7cff2453b7a639e19c0b0fbe70244cf75e268d
Tsinana <hlebusru@gmail.com>
2022-05-08 18:08:49 +0300

    Сделал 6 задание КРАСОТИЩА ТО КАКАЯ

--- LB_8/_5/Program.fs
+++ LB_8/_5/Program.fs
@@ -3,21 +3,29 @@
 open System
 
 ///Паспорт гражданина РФ
-type Id (fullName: string, sex: char, dob: DateTime, nationality: string, bpl: string, series: int, number: int,issued:string,doi:DateTime,department:string,code:int ) =
-    member this.fullName: string = fullName
+type Class_Id (fullName: string, sex: char, dob: DateTime, nationality: string, bpl: string, series: string, number: string,issued:string,doi:DateTime,departmentCode:string ) =
+    member this.fullName: string = fullName.ToUpper()
     member this.sex: char = sex
     member this.dob: DateTime = dob
     member this.nationality: string = nationality
-    member this.bpl: string = bpl
-    member this.series: int = series
-    member this.number: int = number
-    member this.issued:string = issued
+    member this.bpl: string = bpl.ToUpper()
+    member this.series: string = series
+    member this.number: string = number
+    member this.issued:string = issued.ToUpper()
     member this.doi:DateTime = doi
-    member this.department:string = department
-    member this.code: int = code
-     
+    member this.code: string = $"{departmentCode.[0]}{departmentCode.[1]}{departmentCode.[2]}-{departmentCode.[3]}{departmentCode.[4]}{departmentCode.[5]}"
+    
+    member this.Print() = printf$"Паспорт гражданина РФ\n{this.fullName}\n
+Пол      Дата рождения     Гражданство\n{this.sex}        {this.dob.ToShortDateString()}        {this.nationality}\n
+Место рождения\n{this.bpl}\n
+Серия и номер паспорта\n{this.series} {this.number}\n
+Выдан\n{issued}\n
+Дата выдачи     Код подразделения\n{this.doi.ToShortDateString()}      {this.code}\n"
 
 [<EntryPoint>]
 let main argv =
+    let cl1 = Class_Id("Иванов Иван Иваныч", 'M',new DateTime(2000,01,01),"Россия","ГОР. ТУАПСЕ КРАСНОДАРСКОГО КРАЯ","0123","012345","ОТДЕЛОМ УФМС РОССИИ ПО КРАСНОДАРСКОМУ КРАЮ В ТУАПСИНСКОМ РАЙОНЕ",new DateTime(2014,01,01), "012345")
+    cl1.Print()
+
     
     0 // return an integer exit code
