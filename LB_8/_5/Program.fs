2aa0a337e50d3d8cff884bfcf19a733889b8fbca
Tsinana <hlebusru@gmail.com>
2022-05-08 20:46:21 +0300

    Валидация меня валидировала

--- LB_8/_5/Program.fs
+++ LB_8/_5/Program.fs
@@ -1,21 +1,41 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Text.RegularExpressions;
 
 ///Паспорт гражданина РФ
-type Class_Id (fullName: string, sex: char, dob: DateTime, nationality: string, bpl: string, series: string, number: string,issued:string,doi:DateTime,departmentCode:string ) =
+type Class_Id (fullName: string, sex: char, dob: DateTime, nationality: string, bpl: string, series: int, number: int,issued:string,doi:DateTime,departmentCode:int ) =
     member this.fullName: string = fullName.ToUpper()
     member this.sex: char = sex
     member this.dob: DateTime = dob
     member this.nationality: string = nationality
     member this.bpl: string = bpl.ToUpper()
-    member this.series: string = series
-    member this.number: string = number
-    member this.seriesInt: int = Convert.ToInt32(series)
-    member this.numberInt: int = Convert.ToInt32(number)
+    member this.series: string = 
+        match series with
+        |_ when series<10 -> $"000{series}"
+        |_ when series<100 -> $"00{series}"
+        |_ when series<1000 -> $"0{series}"
+        |_ -> $"{series}"
+    member this.number: string = 
+        match number with
+        |_ when number<10 -> $"00000{number}"
+        |_ when number<100 -> $"0000{number}"
+        |_ when number<1000 -> $"000{number}"
+        |_ when number<10000 -> $"00{number}"
+        |_ when number<100000 -> $"0{number}"
+        |_ -> $"{number}"
+    member this.seriesInt: int = series
+    member this.numberInt: int = number
     member this.issued:string = issued.ToUpper()
     member this.doi:DateTime = doi
-    member this.code: string = $"{departmentCode.[0]}{departmentCode.[1]}{departmentCode.[2]}-{departmentCode.[3]}{departmentCode.[4]}{departmentCode.[5]}"
+    member this.code: string = 
+        match departmentCode with
+        |_ when departmentCode<10 -> $"000-00{departmentCode}"
+        |_ when departmentCode<100 -> $"000-0{departmentCode}"
+        |_ when departmentCode<1000 -> $"000-{departmentCode}"
+        |_ when departmentCode<10000 -> $"00{departmentCode/1000}-{departmentCode%1000}"
+        |_ when departmentCode<100000 -> $"0{departmentCode/1000}-{departmentCode%1000}"
+        |_ -> $"{departmentCode/1000}-{departmentCode%1000}"
     //методы
     member this.Compare(s:int,n:int) = if s=this.seriesInt&&n=this.numberInt then this.Print() else printf"Документ не найден\n"
     member this.Compare() = 
@@ -32,10 +52,57 @@ type Class_Id (fullName: string, sex: char, dob: DateTime, nationality: string, 
 Выдан\n{issued}\n
 Дата выдачи     Код подразделения\n{this.doi.ToShortDateString()}      {this.code}\n"
 
+let CreateObjClassId cl= 
+    printf "Начат процесс создания новой записи!\nПри заполнении данных, пожалуйста, используйте только МОГУЧИЕ БУКВЫ и цифры.\n\nВведите ФИО\n"
+    let fullName = Console.ReadLine()
+    let myReg = new Regex("([^А-Яа-я ])|([А-Я][А-Я])|([а-я]+[А-Я])|( [а-я]+)")
+    if myReg.IsMatch(fullName) = false then 
+        printf"Введите пол используя символы 'M' и 'Ж'\n"
+        let sex = Console.ReadLine()
+        let myReg = new Regex("[^М^Ж]")
+        if myReg.IsMatch(sex) = false then 
+            printf "Введите дату рождения как - дд/мм/гггг\n"
+            let dobS = Console.ReadLine()
+            let myReg = new Regex("[^0-9/]")
+            if myReg.IsMatch(dobS) = false then
+                let dob = Convert.ToDateTime(dobS)
+                printf"Введите гражданство\n"
+                let nat = Console.ReadLine()
+                let myReg = new Regex("([^А-Яа-я ])|([А-Я][А-Я])|([а-я]+[А-Я])|( [а-я]+)")
+                if myReg.IsMatch(nat) = false then
+                    printf"Введите место рождения\n"
+                    let bpl = Console.ReadLine()
+                    let myReg = new Regex("[a-zA-Z0-9]")
+                    if myReg.IsMatch(bpl) = false then
+                        printf"Введите серию документа\n"
+                        let series = Convert.ToInt32(Console.ReadLine())
+                        if series<10000&&series>0 then
+                            printf"Введите номер документа\n"
+                            let number = Convert.ToInt32(Console.ReadLine())
+                            if number<1000000&&number>0 then
+                                printf"Введите кем был выдан документ\n"
+                                let issued = Console.ReadLine()
+                                let myReg = new Regex("[a-zA-Z0-9]")
+                                if myReg.IsMatch(issued) = false then
+                                    printf"Введите дату выдачи как - дд/мм/гггг\n"
+                                    let doiS = Console.ReadLine()
+                                    let myReg = new Regex("[^0-9/]")
+                                    if myReg.IsMatch(doiS) = false then
+                                        let doi = Convert.ToDateTime(doiS)
+                                        printf"Введите код выдачи\n"
+                                        let cod = Convert.ToInt32(Console.ReadLine())
+                                        if (cod<1000000)&&(cod>0) then 
+                                            //Class_Id(fullName,sex.[0],dob,nat,bpl,series,number,issued,doi,cod)
+                                            Console.ReadLine()|>ignore
+    Class_Id("Иванов Иван Иваныч", 'M',new DateTime(2000,01,01),"Россия","ГОР. ТУАПСЕ КРАСНОДАРСКОГО КРАЯ",0123,012345,"ОТДЕЛОМ УФМС РОССИИ ПО КРАСНОДАРСКОМУ КРАЮ В ТУАПСИНСКОМ РАЙОНЕ",new DateTime(2014,01,01), 012345)
+
+
+
+
 [<EntryPoint>]
 let main argv =
-    let cl1 = Class_Id("Иванов Иван Иваныч", 'M',new DateTime(2000,01,01),"Россия","ГОР. ТУАПСЕ КРАСНОДАРСКОГО КРАЯ","0123","012345","ОТДЕЛОМ УФМС РОССИИ ПО КРАСНОДАРСКОМУ КРАЮ В ТУАПСИНСКОМ РАЙОНЕ",new DateTime(2014,01,01), "012345")
-    cl1.Compare()
-
-    
+    //let cl1 = Class_Id("Иванов Иван Иваныч", 'M',new DateTime(2000,01,01),"Россия","ГОР. ТУАПСЕ КРАСНОДАРСКОГО КРАЯ",0123,012345,"ОТДЕЛОМ УФМС РОССИИ ПО КРАСНОДАРСКОМУ КРАЮ В ТУАПСИНСКОМ РАЙОНЕ",new DateTime(2014,01,01), 012345)
+    //cl1.Compare()
+    let cl1 = CreateObjClassId()
+    cl1.Print()
     0 // return an integer exit code
